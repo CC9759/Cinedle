da0bc0e14cb7b789409a839908b831b286cd828d
Celina C <cc9759@rit.edu>
2022-02-05 15:47:11 -0500

    Added movie title check

--- imdb_search.py
+++ imdb_search.py
@@ -1,3 +1,6 @@
+"""
+Functions for acquiring imdb information
+"""
 import imdb
 import random
 
@@ -7,6 +10,11 @@ def  get_top250():
     return imdb_data.get_top250_movies()
 
 def get_rand_movie():
-    return get_top250[random.randrange(250)]
+    return get_top250()[random.randrange(250)]
 
-print(get_rand_movie())
+def check_movie(user_input, answer):
+    print(answer)
+    movies = imdb_data.search_movie(user_input)
+
+    return movies != None and movies[0]['title'] == answer['title']
+
