b7116b8c4292826684bdf80750b9c4aab889993a
sz7651 <sz7651@rit.edu>
2022-02-05 13:54:56 -0500

    Implemented guess command that checks whether user input matches actual answer. The actual answer is hard coded in right now.

--- game.py
+++ game.py
@@ -1,22 +1,53 @@
 import os
-import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
+song_name = 'Never Gonna Give You Up'
 
-
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f'{client.user} has descended upon:\n'
-        f'{guild.name}(id: {guild.id})'
+        f'{bot.user} has descended upon:\n'
+        f'{guild.name}(id: {guild.id})\n'
+        'Leggo'
     )
 
-client.run(TOKEN)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content == 'commies' or message.content == 'commie':
+        await message.channel.send('https://www.youtube.com/watch?v=4quwGch9jLg')
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def hi(ctx):
+    user = ctx.message.author
+    await ctx.send("shuddup " + user.display_name)
+
+
+@bot.command()
+async def guess(ctx, *args):
+    await ctx.send('(debug msg) answer: ' + song_name + '\n' +
+                   '(debug msg) user input: ' + ' '.join(args))
+
+    if len(args) == 0:
+        await ctx.send("bruh you didn't even guess. Enter a song name after the command")
+    elif ' '.join(args) == song_name:
+        await ctx.send('wow, you exist!')
+    else:
+        await ctx.send('try asking again')
+
+
+bot.run(TOKEN)
